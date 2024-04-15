26cc5fdc6be0a03821063f500c185a512102982c
JAFFETvr <151978290+JAFFETvr@users.noreply.github.com>
2024-04-14 18:16:55 -0600

    Update Material.java

--- 233436_233302_MULTIDICIPLINARIO/Material.java
+++ 233436_233302_MULTIDICIPLINARIO/Material.java
@@ -1,10 +1,15 @@
 public class Material {
     private String  name ;
     private int cantidad;
+    private String color ;
+    private String tipo;
 
-    public Material(String name, int cantidad) {
+
+    public Material(String name, int cantidad, String color , String tipo) {
         this.name = name;
         this.cantidad = cantidad;
+        this.color=color;
+        this.tipo=tipo;
     }
     
     public int getCantidad() {
@@ -18,6 +23,16 @@ public class Material {
     }
     public void setName(String name) {
         this.name = name;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }public String getColor() {
+        return color;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }public String getTipo() {
+        return tipo;
     }
     
 }
