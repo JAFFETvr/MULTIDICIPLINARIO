ea1f2d16ba1494fd657e09589de65d2d33bdf36e
JAFFETvr <151978290+JAFFETvr@users.noreply.github.com>
2024-04-14 18:16:26 -0600

    Correcion.java

--- 233436_233302_MULTIDICIPLINARIO/Inventario.java
+++ 233436_233302_MULTIDICIPLINARIO/Inventario.java
@@ -3,6 +3,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Inventario {
     private ArrayList<Material> lisMaterial = new ArrayList<>();
+
     Scanner leer = new Scanner(System.in);
     
     public Inventario () {
@@ -17,78 +18,117 @@ public class Inventario {
         return lisMaterial;
     }
 
-    public void inicializarInventario() {
-        lisMaterial.add(new Material("telas", 0));
-        lisMaterial.add(new Material("hilos", 0));
-        lisMaterial.add(new Material("botones", 0));
-        lisMaterial.add(new Material("cierres", 0));
-    }
-
-public void agregarInventario(Scanner leer2) throws InputMismatchException{
-    
-            int opcion=0;
-    int tipoMaterial;
-    do {
-        do {
-            System.out.println("Ingrese el tipo de material que desea agregar: ");
-        System.out.println("1. Telas");
-        System.out.println("2. Hilos");
-        System.out.println("3. Botones");
-        System.out.println("4. Cierres");
-         tipoMaterial = leer2.nextInt();
-
-        } while (tipoMaterial < 1 || tipoMaterial > 4);
+    public void inicializarInventario() {   
         
-
-        String nombreMaterial = "";
-        switch (tipoMaterial) {
-            case 1:
-                nombreMaterial = "telas";
-                break;
-            case 2:
-                nombreMaterial = "hilos";
-                break;
-            case 3:
-                nombreMaterial = "botones";
-                break;
-            case 4:
-                nombreMaterial = "cierres";
-                break;
-            default:
-                System.out.println("Opción no válida");
-                
-        }
-
-        int cantidad;
-        do {
-            System.out.println("Ingrese la cantidad a agregar:");
-            cantidad = leer2.nextInt();
-        } while (cantidad <= 0);
-
-        boolean encontrado = false;
-        for (Material material : lisMaterial) {
-            if (material.getName().equalsIgnoreCase(nombreMaterial)) {
-                encontrado = true;
-                material.setCantidad(material.getCantidad() + cantidad);
-                System.out.println("Cantidad actualizada exitosamente.");
-                break;
-            }
-        }
-        if (!encontrado) {
-            Material nuevoMaterial = new Material(nombreMaterial, cantidad);
-            nuevoMaterial.setName(nombreMaterial);
-            nuevoMaterial.setCantidad(cantidad);
-            lisMaterial.add(nuevoMaterial);
-            System.out.println("Material agregado al inventario.");
-        }
-
-        System.out.println("¿Desea agregar otro material? (1: Sí, 2: No)");
-        opcion = leer.nextInt();
-    } while (opcion == 1);
-          
+    }
     
-}
+    
 
+    public void agregarInventario(Scanner leer2) throws InputMismatchException {
+        int opcion = 0;
+        int tipoMaterial;
+        do {
+            int cantidad=0;
+            do {
+                System.out.println("Ingrese el tipo de material que desea agregar: ");
+                System.out.println("1. Telas");
+                System.out.println("2. Hilos");
+                System.out.println("3. Botones");
+                System.out.println("4. Cierres");
+                tipoMaterial = leer2.nextInt();
+            } while (tipoMaterial < 1 || tipoMaterial > 4);
+    
+            String tipo = "";
+            String color = "";
+            String nombreMaterial = "";
+            
+            switch (tipoMaterial) {
+                case 1:
+                    nombreMaterial = "telas";
+                    System.out.println("Ingrese el tipo de tela (1. Algodon / 2. Acrilico): ");
+                    tipo = leer2.next();
+                    while (!tipo.equals("1") && !tipo.equals("2")) {
+                        System.out.println("Tipo de tela inválido. Ingrese 1 o 2: ");
+                        tipo = leer2.next();
+                    }
+                    System.out.println("Ingrese el color de la tela (rojo, verde, blanco, negro): ");
+                    color = leer2.next();
+                    while (!color.equals("rojo") && !color.equals("verde") && !color.equals("blanco") && !color.equals("negro")) {
+                        System.out.println("Color inválido. Ingrese rojo, verde, blanco o negro: ");
+                        color = leer2.next();
+                    }
+                    do {
+                        System.out.println("Ingrese la cantidad a agregar (Metros):");
+                        cantidad = leer2.nextInt();
+                    } while (cantidad <= 0);
+            
+
+                    break;
+                case 2:
+                    nombreMaterial = "hilos";
+                    System.out.println("Ingrese el tipo de hilo (1. Lino / 2. Ramina): ");
+                    tipo = leer2.next();
+                    while (!tipo.equals("1") && !tipo.equals("2")) {
+                        System.out.println("Tipo de hilo inválido. Ingrese 1 o 2: ");
+                        tipo = leer2.next();
+                    }
+                    System.out.println("Ingrese el color del hilo (rojo, verde, blanco, negro): ");
+                    color = leer2.next();
+                    while (!color.equals("rojo") && !color.equals("verde") && !color.equals("blanco") && !color.equals("negro")) {
+                        System.out.println("Color inválido. Ingrese rojo, verde, blanco o negro: ");
+                        color = leer2.next();
+                    }
+                    do {
+                        System.out.println("Ingrese la cantidad a agregar (Metros):");
+                        cantidad = leer2.nextInt();
+                    } while (cantidad <= 0);
+
+            
+                    break;
+                case 3:
+                    nombreMaterial = "botones";
+                    do {
+                        System.out.println("Ingrese la cantidad a agregar (Metros):");
+                        cantidad = leer2.nextInt();
+                    } while (cantidad <= 0);
+
+                    break;
+                case 4:
+                    nombreMaterial = "cierres";
+                    do {
+                        System.out.println("Ingrese la cantidad a agregar (Metros):");
+                        cantidad = leer2.nextInt();
+                    } while (cantidad <= 0);
+
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+            }
+    
+           
+           
+            boolean encontrado = false;
+            for (Material material : lisMaterial) {
+                if (material.getName().equalsIgnoreCase(nombreMaterial) && material.getColor().equalsIgnoreCase(color) && material.getTipo().equalsIgnoreCase(tipo)) {
+                    encontrado = true;
+                    material.setCantidad(material.getCantidad() + cantidad);
+                    System.out.println("Cantidad actualizada exitosamente.");
+                    break;
+                }
+            }
+            if (!encontrado) {
+                Material nuevoMaterial = new Material(nombreMaterial, cantidad, color, tipo);
+                nuevoMaterial.setName(nombreMaterial);
+                nuevoMaterial.setCantidad(cantidad);
+                lisMaterial.add(nuevoMaterial);
+                System.out.println("Material agregado al inventario.");
+            }
+    
+            System.out.println("¿Desea agregar otro material? (1: Sí, 2: No)");
+            opcion = leer.nextInt();
+        } while (opcion == 1);
+    }
+    
 
 
 public void eliminarInventario()throws InputMismatchException {
@@ -136,6 +176,7 @@ public void eliminarInventario()throws InputMismatchException {
                 }
             }
             
+            
             if (!encontrado) {
                 System.out.println("El material especificado no se encontró en el inventario.");
             }
@@ -152,13 +193,20 @@ public void eliminarInventario()throws InputMismatchException {
     
 }
 
-public void imprimirInventario()throws InputMismatchException {
+public void imprimirInventario() throws InputMismatchException {
     System.out.println("Inventario:");
     for (int i = 0; i < lisMaterial.size(); i++) {
         Material material = lisMaterial.get(i);
-        System.out.println("Nombre: " + material.getName() + ", Cantidad: " + material.getCantidad() );
-        
+        if (material.getCantidad() > 0) {
+            System.out.print("Nombre: " + material.getName() + ", Cantidad: " + material.getCantidad());
+            if (material.getName().equals("telas") || material.getName().equals("hilos")) {
+                System.out.println(", Color: " + material.getColor() + ", Tipo: " + material.getTipo());
+            } else {
+                System.out.println();
+            }
+        }
     }
 }
+
     
 }
